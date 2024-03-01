f5d85b4acdbc0cc46c807d0f36b21c31fa7b458b
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2024-02-29 22:52:43 -0300

    build sucess

--- app/index.tsx
+++ app/index.tsx
@@ -1,103 +1,40 @@
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from "react-native-chart-kit";
+import React, { useRef, useEffect } from 'react';
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import { GridComponent } from 'echarts/components';
+import { SVGRenderer, SkiaChart } from '@wuba/react-native-echarts';
 
-import { AxiosGet } from "@/components/axios/axiosGet";
+echarts.use([SVGRenderer, LineChart, GridComponent]);
 
-import { View, Text, Dimensions } from "react-native";
-import { useEffect, useState } from "react";
-
-export default function ModalScreen() {
-  const [dataFetch, setDataFetch] = useState([]);
-  const E_WIDTH =  Dimensions.get('screen').width
-
-  const chartConfig = {  backgroundGradientFrom: "#1E2923",
-  backgroundGradientFromOpacity: 0,
-  backgroundGradientTo: "#08130D",
-  backgroundGradientToOpacity: 0.5,
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
-  barPercentage: 0.5,
-  useShadowColorFromDataset: false // optional
-  }
-
-  const fetchData = async (ano?: any, mes?: any) => {
-    try {
-      const response = await AxiosGet('atendimentosSexo', {
-        mes: mes || '',
-        ano: ano || '',
+export default function App() {
+  const skiaRef = useRef<any>(null);
+  useEffect(() => {
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line',
+        },
+      ],
+    };
+    let chart: any;
+    if (skiaRef.current) {
+      chart = echarts.init(skiaRef.current, 'light', {
+        renderer: 'svg',
+        width: 400,
+        height: 400,
       });
-      setDataFetch(response.data)
-      
-    } catch (error) {
-      
+      chart.setOption(option);
     }
-  }
+    return () => chart?.dispose();
+  }, []);
 
-  const data = [
-    {
-      name: "Seoul",
-      population: 21500000,
-      color: "rgba(131, 167, 234, 1)",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "Toronto",
-      population: 2800000,
-      color: "#F00",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "Beijing",
-      population: 527612,
-      color: "red",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "New York",
-      population: 8538000,
-      color: "#ffffff",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "Moscow",
-      population: 11920000,
-      color: "rgb(0, 0, 255)",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    }
-  ];
-
-useEffect(() => {
-  fetchData()
-  console.log()
-},[])
-
-  return (
-    <View>
-  {dataFetch&& dataFetch.map((item:any, index)=>(<View key={index}>
-    <Text>{item.SexoDS}</Text>
-  </View>))}
-  <PieChart
-  data={data}
-  width={E_WIDTH}
-  height={220}
-  chartConfig={chartConfig}
-  accessor={"population"}
-  backgroundColor={"transparente"}
-  paddingLeft={"15"}
-  center={[10, 50]}
-  absolute
-/>
-</View>
-  );
+  return <SkiaChart ref={skiaRef} />;
 }
